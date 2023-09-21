6a9ff25a3a34d808eba1ce1ee9b467038bf1decb
AnyySann <86086338+anyy520@users.noreply.github.com>
2023-09-21 00:35:00 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,27 +1,34 @@
-var mouseEvent = "empty";
-var lastPositionOfX, lastPositionOfY;
-color = "black";
-widthOfLine = 2;
+//var mouseEvent = "empty";removeremos a variável mouseEvent, pois não precisamos dela agora.
+var lastPositionOfX, lastPositionOfY;//As variáveis que utilizamos para armazenar as coordenadas X e Y permanecerão as mesmas.
 
-canvas = document.getElementById('myCanvas');
-ctx = canvas.getContext("2d");
+color = "black";//para a variável que contém color e para a variável que
+widthOfLine = 2;//contém a width, serão as mesmas.
 
-var width = screen.width;
+canvas = document.getElementById('myCanvas');//referencia canvas
+ctx = canvas.getContext("2d");//atribui a varivel ctx
 
-newWidth =  screen.width - 70; 
-newHeight = screen.height - 300;
+var width = screen.width;//buscaremos a width da tela e a armazenamos dentro da variável.
+//pois queremos saber qual tamanho de tela (mobile ou desktop) que o usuário está utilizando.
+newWidth =  screen.width - 70; // definiremos uma variável newWidth:● Essa variável será 
+                              //utilizada para armazenar a nova width que será obtida aosubtrairmos a screen.width de 70 da width da tela do usuário.
+                             //○ Esse 70 significa 70px.
+newHeight = screen.height - 300;//armazenar a nova height, que será obtida subtraindo screen.height de 300
+// if abaixo
+//Se o tamanho da tela for menor que 992 (ou seja, 992px), então, modifique
+//a width e a height do canvas para as novas width e height, as quais
+//definimos no ponto acima
 
-	if(width < 992)
+     if(width < 992)//
 	{
 	document.getElementById("myCanvas").width = newWidth;
-    document.getElementById("myCanvas").height = newHeight;
-    document.body.style.overflow = "hidden";
+       document.getElementById("myCanvas").height = newHeight;
+       document.body.style.overflow = "hidden";
 	}
 
-canvas.addEventListener("touchstart", my_touchstart);
+canvas.addEventListener("touchstart", my_touchstart);//
 
 
-function my_touchstart(e) 
+function my_touchstart(e) //
 {
 	console.log("my_touchstart");
   //Atividade Adicional
@@ -33,7 +40,7 @@ function my_touchstart(e)
     lastPositionOfY = e.touches[0].clientY - canvas.offsetTop;
 }
 
-canvas.addEventListener("touchmove", my_touchmove);
+canvas.addEventListener("touchmove", my_touchmove);//
 function my_touchmove(e) 
 {
 
